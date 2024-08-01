23696faa0a0713380d4e898943b5090791f96337
Volodymyr Korzhevskyi <korzhevskyiv@gmail.com>
2024-08-01 09:52:32 +0100

    feat(userAccountRepository): make search users by email

--- ServerLibrary/Repositories/Implementations/UserAccountRepository.cs
+++ ServerLibrary/Repositories/Implementations/UserAccountRepository.cs
@@ -1,24 +1,31 @@
 ﻿using ServerLibrary.Repositories.Contracts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using ServerLibrary.Helpers;
 using BaseLibrar.Responses;
+using ServerLibrary.Data;
 using BaseLibrar.DTOs;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using BaseLibrar.Entities;
+
 
 namespace ServerLibrary.Repositories.Implementations
 {
-    internal class UserAccountRepository : IUserAccount
+    internal class UserAccountRepository(IOptions<JwtSection> config, AppDbContext appDbContext) : IUserAccount
     {
-        public Task<GeneralResponse> CreateAsync(Register user)
+         public async Task<GeneralResponse> CreateAsync(Register user)
         {
-            throw new NotImplementedException();
+            if (user is null) return new GeneralResponse(false, "Model is empty");
+
+            var checkUser = await FindUserByEmail(user.Email);
+            if (checkUser != null) return new GeneralResponse(false, "User registered already");
         }
 
         public Task<LoginResponse> SignInAsync(Login user) 
         { 
             throw new NotImplementedException(); 
         }
+
+        private async Task<ApplicationUser> FindUserByEmail(string email) => 
+            await appDbContext.ApplicationUsers.FirstOrDefaultAsync(_ => _.Email!.ToLower()!.Equals(email!.ToLower() ) );
     }
 }
